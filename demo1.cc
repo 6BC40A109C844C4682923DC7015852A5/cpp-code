648c3bd813b9c011cc69f2673c85c7041e29314a
Troy Song <515483037@qq.com>
2020-03-25 19:42:51 +0800

    Depth-first Traversal (recursive)
    Resolve #9
    
    Signed-off-by: Troy Song <515483037@qq.com>

--- demo1.cc
+++ demo1.cc
@@ -1,11 +1,45 @@
+#include "Btree.h"
 #include "gtest/gtest.h"
 
-TEST(Suite1, Test1) {
-  int res = 1 + 2;
-  EXPECT_EQ(res, 3);
+TEST(Suite1, test){
+    Btree<int> bt;
+    LinkList<int> prl;
+    LinkList<int> inl;
+    LinkList<int> pol;
+    int pre[] = {11, 22, 44, 77, 55, 88};
+    int in[] = {44, 77, 22, 11, 55 ,88};
+    int pos[] = {77, 44, 22, 88, 55, 11};
+
+    bt.PreInCreate(pre, in, 5, 5);
+
+    bt.PreOrder(prl);
+    int i1 = 0;
+    for(int d : prl){
+        EXPECT_EQ(d, pre[i1]);
+        i1++;
+    }
+    EXPECT_EQ(i1, 6);
+
+    bt.InOrder(inl);
+    int i2 = 0;
+    for(int d : inl){
+        EXPECT_EQ(d, in[i2]);
+        i2++;
+    }
+    EXPECT_EQ(i2, 6);
+
+    bt.PostOrder(pol);
+    int i3 = 0;
+    for(int d : pol){
+        EXPECT_EQ(d, pos[i3]);
+        i3++;
+    }
+    EXPECT_EQ(i3, 6);
+
 }
 
+
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
